7b34f318ab106e05c5e36672bfec9be1b4cca113
Noah <lauffern@gmail.com>
2022-04-04 12:49:07 +0000

    init commmit

---
+++ two-o-p.rb
@@ -0,0 +1,22 @@
+  # Player class
+  # 
+  # will create a new player initalized with a certain set of critera to make playing-field even. Will have a name variable, and a score variable, also contains a state(score) checker If score is too low, will end the game and declare a winner. 
+  # 
+  # each created with a score of 3/3
+  #
+  # each has name  
+  # 
+  # func to check status of score
+
+
+
+  # Question Class 
+  # 
+  # to create randomized simple math questions and deduct if they are correct or not, and take the proper action of subtracting points from each player. Will contain game loop. 
+  # 
+  # will need a randomized numbers variable 
+  # 
+  #  will need a question template with temp literals to ensure question is different each time. 
+  # 
+  #  
+
